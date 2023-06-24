f2029ccb6916ce4fa1c6bdd74266d8a160c5a3aa
yonbok <na.re.zutomayo14@gmail.com>
2023-06-24 06:04:43 +0000

    AssociationNotFoundError

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,6 +32,16 @@ class UsersController < ApplicationController
     end
   end
 
+  def followings
+    @user = User.find(params[:user_id])
+    users = user.followings
+  end
+
+  def followers
+    @user = User.find(params[:user_id])
+    users = user.followers
+  end
+
   private
 
   def user_params
