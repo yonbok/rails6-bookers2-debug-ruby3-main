a1116fdf78e792e4ef4a1545c393e8dcdd14cb9e
yonbok <na.re.zutomayo14@gmail.com>
2023-07-01 06:56:54 +0000

    [update]user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
 
   has_many :followings, through: :relationships, source: :followed
-  has_many :followers, through: :reverse_of_relationships, source: :folllower
+  has_many :followers, through: :reverse_of_relationships, source: :follower
   has_one_attached :profile_image
 
 
@@ -27,7 +27,7 @@ class User < ApplicationRecord
   validates :introduction,  length: { maximum: 50 }
 
 
-  def follow(followed_id: user_id)
+  def follow(user_id)
     relationships.create(followed_id: user_id)
   end
 
