4995e3ac09ee461cbe24ab8b71a4506dcd594ee8
yonbok <na.re.zutomayo14@gmail.com>
2023-07-03 11:16:06 +0000

    [add]search page

--- app/models/book.rb
+++ app/models/book.rb
@@ -19,4 +19,19 @@ class Book < ApplicationRecord
     favorites.exists?(user_id: user.id)
   end
 
+
+  def self.search(search, word)
+    if search == "forward_match"
+                  @book = Book.where("title LIKE?","#{word}%")
+    elsif search == "backward_match"
+                  @book = Book.where("title LIKE?","%#{word}%")
+    elsif search == "perfect_match"
+                  @book = Book.where("#{word}%")
+    elsif search == "partial_match"
+                  @book = Book.where("title LIKE?","%#{word}%")
+    else
+                  @book = Book.all
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get "home/about"=>"homes#about"
+  get "search" => "searches#search"
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resource :favorites, only: [:create,:destroy]
