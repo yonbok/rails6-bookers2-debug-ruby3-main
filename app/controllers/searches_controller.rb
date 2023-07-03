e6dfed4526344a1e21eec211610e633738fa8a9c
yonbok <na.re.zutomayo14@gmail.com>
2023-07-03 04:21:03 +0000

    [update]user model

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,15 @@
 class SearchesController < ApplicationController
 
   def search
+    @range = params[:range]
+    search = params[:seaech]
+    word = params[:word]
+
+    if @range == "1"
+      @user = User.search(search,word)
+    else
+      @book = Book.search(search,word)
+    end
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -39,4 +39,19 @@ class User < ApplicationRecord
     followings.include?(user)
   end
 
+
+  def self.search(search,word)
+    if search == "forward_match"
+                 @user = User.where("name LIKE?","#{word}%")
+    elsif search == "backward_match"
+                 @user = User.where("name LIKE?","%#{word}%")
+    elsif search == "perfect_match"
+                 @user = User.where("#{word}%")
+    elsif search == "partial_match"
+                 @user = User.where("name LIKE?","%#{word}%")
+    else
+                 @user = User.all
+    end
+  end
+
 end
