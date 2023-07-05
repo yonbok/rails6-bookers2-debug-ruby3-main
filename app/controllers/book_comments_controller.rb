1c24d32bc56e00d875c2f3f311bc4e61f2cad269
yonbok <na.re.zutomayo14@gmail.com>
2023-07-05 06:11:29 +0000

    [add]コメント機能の非同期通信

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -5,12 +5,12 @@ class BookCommentsController < ApplicationController
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
     comment.save
-    redirect_to book_path(book)
+    redirect_to request.referer
   end
 
   def destroy
     BookComment.find(params[:id]).destroy
-    redirect_to book_path(params[:book_id])
+    redirect_to request.referer
   end
 
 
