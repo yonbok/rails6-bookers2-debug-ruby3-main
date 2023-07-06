eef8861b0a7d927f314626f6577013cca5485cdc
yonbok <na.re.zutomayo14@gmail.com>
2023-07-06 15:22:41 +0000

    [update]user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,29 @@
 class UsersController < ApplicationController
-  #before_action :ensure_correct_user, only: [:update]
+  before_action :authenticate_user!, only: [:show]
 
   def show
     @user = User.find(params[:id])
      flash[:notice] = "Welcome! You have signed up successfully."
     @books = @user.books
     @book = Book.new
+    @currentUserEntry=Entry.where(user_id: current_user.id)
+    @userEntry=Entry.where(user_id: @user.id)
+    if @user.id == current_user.id
+    else
+      @currentUserEntry.each do |cu|
+        @userEntry.each do |u|
+          if cu.room_id == u.room_id then
+            @isRoom = true
+            @roomId = cu.room_id
+          end
+        end
+      end
+      if @isRoom
+      else
+        @room = Room.new
+        @entry = Entry.new
+      end
+    end
   end
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,8 @@ Rails.application.routes.draw do
     get "followers" => "relationships#followers", as: "followers"
   end
 
+resources :messages, only: [:create]
+resources :rooms, only: [:create,:show]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
