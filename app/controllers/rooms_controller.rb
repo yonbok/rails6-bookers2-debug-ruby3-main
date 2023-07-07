19a6a99e738d7cd7299054d63a42ddd1fb710409
yonbok <na.re.zutomayo14@gmail.com>
2023-07-07 06:32:27 +0000

    [update]show page

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,2 +1,22 @@
 class RoomsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @room = Room.create(user_id: current_user.id)
+    @entry1 = Entry.create(:room_id => @room.id, :user_id => current_user.id)
+    @entry2 = Entry.create(params.require(:entry).permit(:user_id, :room_id).merge(:room_id => @room.id))
+    redirect_to "/rooms/#{@room.id}"
+  end
+
+  def show
+    @room = Room.find(params[:id])
+    if Entry.where(:user_id => current_user.id, :room_id => @room.id)
+      @messages = @room.messages
+      @message = Message.new
+      @entries = @room.entries
+      @myUserId = current_user.id
+    else
+      redirect_back(fallback_location: root_path)
+    end
+  end
 end
